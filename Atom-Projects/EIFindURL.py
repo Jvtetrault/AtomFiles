89f4f92b4d76c0e402d448a4ae87c7b91e4e3406
Jvtetrault <35230553+Jvtetrault@users.noreply.github.com>
2018-01-09 11:44:44 -0500

    Commit 1

--- Atom-Projects/EIFindURL.py
+++ Atom-Projects/EIFindURL.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import sys
+import EIGrabClass
 
 print(sys.executable)
 print(sys.version)
@@ -9,14 +10,18 @@ print(sys.version)
 # Beginning of Tree Parsing to find specific objects
 url = "http://wiki.inovkh.com/doku.php?id=xpr_component_table"
 page = requests.get(url)
-soup = BeautifulSoup(page.content)
+soup = BeautifulSoup(page.content, "html.parser")
 
 # Finds all links in page
 RawLinks = soup.find_all("a")
 
-# Only uses links to maintenance manual
+
+# Test Out the code by printing
+# Rolls through each link from the URL page
+# Find maintenance items
+# Call EdgeTIGrabber class to download specific file
 for link in RawLinks:
-    if "http" in link.get("href"):
+    if "doku.php?id=item" in link.get("href"):
         print("<a href='%s'>%s</a>" % (link.get("href"), link.text))
 
 # soup2 = BeautifulSoup(page.text, 'html.parser')
@@ -30,3 +35,9 @@ for link in RawLinks:
 # Create for loop to print out all artists' name
 # for test_pull in test_pull_items:
 #    print(test_pull.prettify())
+
+# Sorting throught the list
+for link in RawLinks:
+    if "doku.php?id=item" in link.get("href"):
+        # Call EdgeTiGrabber function
+        # Transform EdgeTiGrabber into Class
---
+++ Atom-Projects/EIGrabClass.py
@@ -0,0 +1,45 @@
+import requests
+import time
+import errno
+import os
+
+
+class EIGrab:
+
+
+    pic_url = 'http://wiki.inovkh.com/doku.php?id=maintenance_procedure&do=export_pdf'
+
+
+    baseFolder = 'F:\EdgeTiLog'
+    datestr = time.strftime("%Y%m%d")
+    timestr = time.strftime("%H%M%S") + '.pdf'
+
+    def mkdir_p(path):
+        try:
+            os.makedirs(path)
+        except OSError as exc:  # Python >2.5
+            if exc.errno == errno.EEXIST and os.path.isdir(path):
+                pass
+            else:
+                raise
+
+# figure out where to save the screengrab
+    dayPath = os.path.join(baseFolder, datestr)
+    fullPicPath = os.path.join(dayPath, timestr)
+
+    mkdir_p(dayPath)
+
+# Get the image from the edgeTi web app and save to computed location
+    try:
+        response = requests.get(pic_url, stream=True, timeout=2)
+
+        if response.ok:
+            with open(fullPicPath, 'wb') as handle:
+                for block in response.iter_content(1024):
+                    if not block:
+                        break
+                        handle.write(block)
+    except:
+        print('fail')
+
+    pass
