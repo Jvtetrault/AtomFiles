d351834bc7c2e87f7c0e61718505ca033b663198
Jvtetrault <35230553+Jvtetrault@users.noreply.github.com>
2018-06-18 15:07:33 -0400

    Critical Update
    Fully functioning maintenance profile puller.

---
+++ Interpreter.py
@@ -0,0 +1,65 @@
+# Simple Interpreter object definition for varying text to number conversion
+
+def interpret(string):
+    # Checks argument
+    a = ((string.lower()).replace(" ", "")).strip()
+    if a == "daily":
+        return 1
+    elif a == "weekly":
+        return 7
+    elif a == "7days":
+        return 7
+    elif a == "bi-weekly":
+        return 14
+    elif a == "biweekly":
+        return 14
+    elif a == "monthly":
+        return 31
+    elif a == "semi-annually":
+        return 183
+    elif a == "quarterly":
+        return 91
+    elif a == "quaterly":
+        return 91
+    elif a == "semiannually":
+        return 183
+    elif a == "yearly":
+        return 365
+    elif a == "1years":
+        return 365
+    elif a == "1year":
+        return 365
+    elif a == "2years":
+        return 730
+    elif a == "3years":
+        return 1095
+    elif a == "4years":
+        return 1460
+    elif a == "5years":
+        return 1825
+    elif a == "6years":
+        return 2190
+    elif a == "-":
+        return 0
+    elif a == "7Days":
+        return 7
+    elif a == "firstmonth":
+        return 31
+    elif a == "6months":
+        return 186
+    elif a == "9months":
+        return 279
+    elif a == "1year":
+        return 365
+    elif a == "2years":
+        return 730
+    elif a == "3years":
+        return 1095
+    elif a == "6years":
+        return 2190
+    elif a == "5years":
+        return 1825
+    else:
+        # Imprompt to non-stick exception, will have to define Exception block to limit and kill process when exception is thrown.
+        # Maybe add an exterior option to modify list when time presents itself to handle new items.
+        print('"'+ string + '" Is not a passable argument. Please view documentation of Interpreter: Passed string is outside library of defined passable arguments')
