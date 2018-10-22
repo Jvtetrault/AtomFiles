3b66e78c401b25548694cf8c38f081f75dc71e19
Jvtetrault <35230553+Jvtetrault@users.noreply.github.com>
2018-10-22 10:57:20 -0400

    Minor Changes
    Minor Directory cleanup

---
+++ EIFindURL.py
@@ -0,0 +1,155 @@
+from Tkinter import Tk
+from tkFileDialog import askdirectory
+from bs4 import BeautifulSoup
+import requests
+import easygui
+import time
+import errno
+import os
+import sys
+import Xmlm
+import Interpreter as i
+import Load
+import argparse
+
+
+# Function to accept URL and Destination folder arguments
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url", help="Provide Table destination URL from DocuWiki")
+    parser.add_argument("destination", help="Provide destination for the XML and PDF files to download")
+
+    args = parser.parse_args()
+
+    url = args.url
+    location = args.destination
+
+else:
+    url = easygui.enterbox(
+        msg="Paste Table URL here",
+        title="Table URL Retrieval",
+        strip=True,
+        default="")
+
+    Tk().withdraw()
+    location = askdirectory()
+
+
+# Open user inquiery box for URL string
+# url = easygui.enterbox(
+#     msg="Paste Table URL here",
+#     title="Table URL Retrieval",
+#     strip=True,
+#     default="")
+
+
+# Transforms HTML Tree into easily navigatable Python Tree.
+page = requests.get(url)
+# Beginning of Tree Parsing to find specific objects
+soup = BeautifulSoup(page.content, "html.parser")
+# Allows user to pick file destination
+# Tk().withdraw()
+# location = askdirectory()
+# Creates object with all HTML links <a> from URL after passing through requests
+# and BeautifulSoup
+RawLinks = soup.find_all("a")
+
+
+# Main Download function
+def getFile(ChosenUrl, Chosenlocation, fileName):
+    baseFolder = Chosenlocation
+    pic_url = ChosenUrl
+    nfile = fileName.replace("item_", "") + '.pdf'
+    datestr = "en-US"
+
+    def mkdir_p(path):
+        try:
+            os.makedirs(path)
+        except OSError as exc:  # Python >2.5
+            if exc.errno == errno.EEXIST and os.path.isdir(path):
+                pass
+            else:
+                raise
+
+    # figures out where to place the folder and name it
+    dayPath = os.path.join(baseFolder, datestr)
+    # Generates directory with name as timestamp
+    mkdir_p(dayPath)
+
+    # Created path for file to be dropped into new directory
+    # Creates path type C:\**Basefolderpath\filename.pdf
+    filePath = os.path.join(dayPath, nfile)
+
+    # Load.pbar(filePath, pic_url)
+    # Get the image from the edgeTi web app and save to computed location
+
+    try:
+        response = requests.get(pic_url, stream=True, timeout=100)
+
+        if response.ok:
+            with open(filePath, 'wb') as handle:
+                for block in response.iter_content(1024):
+                    if not block:
+                        break
+                    handle.write(block)
+    except:
+        print('fail')
+
+
+
+
+# Main Script
+
+
+
+    # Download Loop
+
+
+for link in RawLinks:
+    if "/doku.php?id=item" in link.get("href"):
+
+
+        # Combines into simple read request pulling for pdf export.
+        # Seperate forms of URL with/wuthout export function
+        PartialUrl = "http://wiki.inovkh.com/" + link.get("href")
+        FullUrl = PartialUrl  + "&do=export_pdf"
+
+
+        # Tags URL of target file
+        response = requests.get(FullUrl, stream=True, timeout=1000)
+        fileName = link.get("title")
+
+
+        # Pull in getFile() object function
+        getFile(FullUrl, location, fileName)
+        print("Item: " + fileName.replace("item_", "") + " Downloaded.")
+
+
+
+
+# Builds blank XML file in the User chosen location.
+maintenancefile = str(Xmlm.build(location))
+print(maintenancefile)
+
+
+# XML Generation/Appending Loop
+for string in soup.find_all('tr'):
+    for Link in string:
+        if '<td class="col0"><a class="wikilink1" href=' in str(Link):
+
+            temp = link.get("title")
+            Id = (fileName.replace("item_", ""))
+            print(Id)
+        else:
+            if '<td class="col1' in str(Link):
+                dur = i.interpret(str(Link.get_text()))
+            else:
+                if '<td class="col2' in str(Link):
+                    ofs = i.interpret(str(Link.get_text()))
+                else:
+                    if '<td class="col3">' in str(Link):
+                        name = Link.get_text()
+                        Xmlm.write1(Id, dur, ofs, name, 'PDF', Id, maintenancefile)
+Xmlm.finish(maintenancefile)
+print("Maintenance.xml file created.")
---
+++ Xmlm.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python
+# import lxml.etree
+# import lxml.builder
+# from lxml import etree
+# import xml.etree.ElementTree as ET
+# from xml.etree.ElementTree import ElementTree
+# from xml.etree.ElementTree import Element
+
+
+# E = lxml.builder.ElementMaker()
+# ROOT = E.Tasks
+# DOC = E.Task
+# ID = E.ID
+# Period = E.Period
+# Offset = E.Offset
+# Task = E.Task
+# Help = E.Help
+# URL = E.URL
+
+
+def write1(id, period, offset, task, help, name, files):
+    Path = "C:/ProgramData/Inovatech Engineering Corp/SteelPRO Director/Documentation/Maintenance/en-US/"
+    dest = str(Path + name + ".pdf")
+    newtask = task.replace("&", "and")
+    ID8 = id.encode("utf-8")
+    PER8 = str(period)
+    OFF8 = str(offset)
+    TAS8 = newtask.encode("utf-8")
+    HEL8 = help
+
+    items = [ID8, PER8, OFF8, TAS8, HEL8, dest]
+    titles = ["ID", "Period", "Offset", "Task", "Help", "URL"]
+
+    # openfile = open(files, 'a')
+    # tree = ET.parse(openfile)
+    # root = tree.getroot()
+    #
+    # doc = root.append("Task")
+    openfile = open(files, "a")
+    openfile.write(("  <Task>\n").encode("utf-8"))
+    for i in range(len(items)):
+        a = "    <" + titles[i] + ">" + items[i] + "</" + titles[i] + ">"
+        openfile.write(a + "\n")
+    openfile.write(("  </Task>\n").encode("utf-8"))
+
+
+
+    # tree = etree.parse(files)
+    # root = tree.getroot()
+
+    # a = root.find('Tasks')
+    # Doc = a.etree.SubElement("Task")
+    # ID = etree.SubElement(Doc, str(id))
+    # Period = etree.SubElement(Doc, str(period))
+    # Task = etree.SubElement(Doc, str(task))
+    # Help = etree.SubElement(Doc, str(help))
+    # Name = etree.SubElement(Doc, str(name))
+
+
+
+
+
+    # the_doc = ROOT(
+    #     DOC(
+    #         ID(str(id)),
+    #         Period(str(period)),
+    #         Offset(str(offset)),
+    #         Task(str(task)),
+    #         Help(str(help)),
+    #         URL(str(dest))
+    #
+    #     )
+    # )
+    # print lxml.etree.tostring(the_doc, pretty_print=True)
+
+
+def build(path):
+        destination = path + "/Maintenance.xml"
+        file = open(destination, "w")
+        file.write(('<?xml version="1.0" encoding="UTF-8"?>').encode("utf-8"))
+        file.write("\n")
+        file.write(("<Tasks>\n").encode("utf-8"))
+        file.close()
+        return str(destination)
+        # root = etree.Element("Tasks")
+        # tree = etree.ElementTree(root)
+        # tree.write("C:/Users/Jonathan Tetrault/Desktop/New folder/filename.xml")
+
+def finish(files):
+    openfile = open(files, "a")
+    openfile.write(("  <Task>\n").encode("utf-8"))
+    openfile.write(("    <ID/>\n").encode("utf-8"))
+    openfile.write(("  </Task>\n").encode("utf-8"))
+    openfile.write(("  <Task>\n").encode("utf-8"))
+    openfile.write(("    <ID/>\n").encode("utf-8"))
+    openfile.write(("  </Task>\n").encode("utf-8"))
+    openfile.write(("  <Task>\n").encode("utf-8"))
+    openfile.write(("    <ID/>\n").encode("utf-8"))
+    openfile.write(("  </Task>\n").encode("utf-8"))
+    openfile.write(("  <Task>\n").encode("utf-8"))
+    openfile.write(("    <ID/>\n").encode("utf-8"))
+    openfile.write(("  </Task>\n").encode("utf-8"))
+    openfile.write(("</Tasks>").encode("utf-8"))
+    openfile.close()
+
+
+# do = build("C:\Users\Jonathan Tetrault\Desktop")
+# write1("1","1","1","1",'PDF',"1",do)
+# finish(do)
